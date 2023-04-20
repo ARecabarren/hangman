308cdc131df3c9b0a8e9840fac9392e20928961d
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-04-20 16:12:36 -0400

    Implement load game

--- lib/save_game.rb
+++ lib/save_game.rb
@@ -2,7 +2,7 @@ require 'json'
 module Manage_games
 
     def save_game
-        
+        write_status
     end
 
     def write_status
@@ -19,11 +19,30 @@ module Manage_games
         username = gets.chomp
         filename = "../games/#{username}_game.json"
         File.open(filename,'w') do |file|
-            file.puts game_status
+            file.puts game_status.to_json
         end
     end
 
-    def load_game(user_input)
-        
+    def load_game
+        puts "Choose a game by it's numeric index '[number]'"
+        display_saved_games
+        game_to_load = gets.chomp.to_i
+        data = read_file(game_to_load)
+        data
+    end
+
+    def display_saved_games
+        games_array = Dir.glob('../games/*.json')
+        games_array.each_with_index do |game,index|
+            user_name = File.basename(game,"_game.json")
+            puts "[#{index}] #{user_name}"
+        end
+    end
+
+    def read_file(game_to_load)
+        games_array = Dir.glob('../games/*.json')
+        directory = games_array[game_to_load]
+        saved_game = File.read("#{directory}")
+        JSON.parse(saved_game)
     end
 end
